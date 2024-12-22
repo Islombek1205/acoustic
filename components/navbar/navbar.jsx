f49c6ea522cfbbdb37fdf363d6dfdcb703d3d22a
Islombek1205 <islombekobidov40@gmail.com>
2024-12-22 17:50:38 +0500

    nav ga media yozildi va home page yaratilmoqda

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,15 +1,36 @@
 import { IoSearch } from "react-icons/io5";
 import { FaPhoneAlt } from "react-icons/fa";
-import { FaChevronDown } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";    
 import Link from "next/link";
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+    }
+
+    const closeMenu = (e) => {
+        if (menuRef.current && !menuRef.current.contains(e.target)) {
+          setIsMenuOpen(false);
+        }
+      };
+
+    useEffect(() => {
+         document.addEventListener("mousedown", closeMenu);
+    
+        return () => {
+          document.removeEventListener("mousedown", closeMenu);
+        };
+      }, []);
+
   return (
     <div className='navContainer'>
         <div className="nav-top">
-            <img src="/logo-acoustic.png" alt="logo" width={150}/>
+            <img src="/logo-acoustic.png" alt="logo"/>
             <div className='search-form'>
                 <input type="text" />
                 <button type="submit" className="search-btn"><IoSearch className="search-icon"/></button>
@@ -17,10 +38,10 @@ function Navbar() {
             <div className="new-infoline">
                 <div className="main-phone">
                     <FaPhoneAlt className="phone-icon"/>
-                    <a href="#">+998712021441</a>
+                    <a href="#">+99871 202-14-41</a>
                 </div>
-                <button className="chervon-down"><FaChevronDown /></button>
             </div>
+            <button className="bar-btn" onClick={toggleMenu}><FaBars /></button>
         </div>
         <div className="nav-bottom">
             <ul>
@@ -33,6 +54,17 @@ function Navbar() {
                 <li><Link href={'/centers'} className="pages">Наши адреса</Link></li>
             </ul>
         </div>
+        {isMenuOpen && (
+            <ul className="navbar-menu" ref={menuRef}>
+                <li><Link href={'/services'} className="pages">Услуги</Link></li>
+                <li><Link href={'/catalog'} className="pages">Каталог</Link></li>
+                <li><Link href={'/doctors'} className="pages">Врачи</Link></li>
+                <li><Link href={'/tinnitus'} className="pages">Пациентам</Link></li>
+                <li><Link href={'/detiISluh'} className="pages">Дети и слух</Link></li>
+                <li><Link href={'/aboutUs'} className="pages">О нас</Link></li>
+                <li><Link href={'/centers'} className="pages">Наши адреса</Link></li>
+            </ul>
+        )}
     </div>
   )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function Index() {
   return (
-    <div>Index</div>
+    <div home-container>
+        <div className="slider">
+            
+        </div>
+        <div className="for-content">
+            <h1>Наши услуги</h1>
+            <div className="grid-cards">
+                <div className="card">
+                    <img src="/diagnostika.png.webp" alt="diagnostika" />
+                    <h2>Диагностика слуха у взрослых</h2>
+                    <p>Все виды обследования слуха на новейшем оборудовании Interacoustics (Дания)</p>
+                </div>
+                <div className="card">
+                    <img src="/deti.png.webp" alt="diagnostika" />
+                    <h2>Диагностика слуха у детей</h2>
+                    <p>Новейшие методы обследования слуха у детей разного возраста, в т.ч. новорожденных</p>
+                </div>
+                <div className="card">
+                    <img src="/otoplastik-1.png.webp" alt="diagnostika" />
+                    <h2>Ушные вкладыши</h2>
+                    <p>Изготовление индивидуальных ушных вкладышей для слуховых аппаратов взрослым и детям</p>
+                </div>
+                <div className="card">
+                    <img src="/our_adresses.png.webp" alt="diagnostika" />
+                    <h2>Найти ближайший центр</h2>
+                    <p>Адреса медицинских центров диагностики и коррекции слуха в Беларуси</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
